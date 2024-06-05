5f5a5c535a5d4aa3a3b6046479ad8333352d4700
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-06-05 19:47:31 +0900

    [feat/#2] feat: 숙소 기본 클래스 생성

---
+++ src/main/java/com/airdnb/clone/domain/stay/Stay.java
@@ -0,0 +1,43 @@
+package com.airdnb.clone.domain.stay;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Stay {
+
+    private Long id;
+    private Long hostId;
+    private String alias;
+    private String location;
+    private Integer perNight;
+    private Integer cleaningFee;
+    private LocalTime checkInTime;
+    private LocalTime checkOutTime;
+    private String roomType;
+    private String available;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder.Default
+    private Integer guestCount = 1;
+
+    @Builder.Default
+    private Integer bedroomCount = 0;
+
+    @Builder.Default
+    private Integer bedCount = 1;
+
+    @Builder.Default
+    private Integer bathCount = 0;
+
+}
