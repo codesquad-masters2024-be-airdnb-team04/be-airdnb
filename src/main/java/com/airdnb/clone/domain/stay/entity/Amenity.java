3df03ccca3374fa17f0ae00f3c18badf3e7efe0b
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-06-06 14:57:04 +0900

    [feat/#2] feat: 생성자에 Builder 추가

--- src/main/java/com/airdnb/clone/domain/stay/entity/Amenity.java
+++ src/main/java/com/airdnb/clone/domain/stay/entity/Amenity.java
@@ -1,6 +1,7 @@
 package com.airdnb.clone.domain.stay.entity;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ public class Amenity {
     private String name;
     private String description;
 
+    @Builder
     public Amenity(String mainCategory, String name, String description) {
         this.mainCategory = mainCategory;
         this.name = name;
