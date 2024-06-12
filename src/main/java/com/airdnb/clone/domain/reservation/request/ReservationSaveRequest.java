3cd087c8a0b09250df66cb8fc2af1f51a65614a8
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-06-12 14:02:30 +0900

    [feat/#13] feat: dto에 유효성 검증 추가

--- src/main/java/com/airdnb/clone/domain/reservation/request/ReservationSaveRequest.java
+++ src/main/java/com/airdnb/clone/domain/reservation/request/ReservationSaveRequest.java
@@ -4,14 +4,29 @@ import com.airdnb.clone.domain.common.Guest;
 import com.airdnb.clone.domain.member.entity.Member;
 import com.airdnb.clone.domain.reservation.entity.Reservation;
 import com.airdnb.clone.domain.stay.entity.Stay;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public record ReservationSaveRequest(
+
+        @NotNull
         Long memberId,
+
+        @NotNull
         Long stayId,
+
+        @Future
         LocalDateTime checkIn,
+
+        @Future
         LocalDateTime checkOut,
+
+        @Min(value = 0)
         Long totalRate,
+
+        @Min(value = 1)
         Integer guestCount
 ) {
     public Reservation toEntity(Member member, Stay stay) {
--- src/main/java/com/airdnb/clone/domain/reservation/request/ReservationUpdateRequest.java
+++ src/main/java/com/airdnb/clone/domain/reservation/request/ReservationUpdateRequest.java
@@ -1,6 +1,9 @@
 package com.airdnb.clone.domain.reservation.request;
 
+import jakarta.validation.constraints.Min;
+
 public record ReservationUpdateRequest(
+        @Min(value = 1)
         Integer guestCount
 ) {
 }
