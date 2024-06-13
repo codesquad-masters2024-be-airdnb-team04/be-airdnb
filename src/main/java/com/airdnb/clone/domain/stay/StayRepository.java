58cadebba3e2c809114b9faeb8a82564473d6108
yeriimii <87357932+Yeriimii@users.noreply.github.com>
2024-06-13 11:04:13 +0900

    Update src/main/java/com/airdnb/clone/domain/stay/StayRepository.java
    [feat/#13] style: 코드 컨벤션 적용

--- src/main/java/com/airdnb/clone/domain/stay/StayRepository.java
+++ src/main/java/com/airdnb/clone/domain/stay/StayRepository.java
@@ -3,5 +3,6 @@ package com.airdnb.clone.domain.stay;
 import com.airdnb.clone.domain.stay.entity.Stay;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface StayRepository extends JpaRepository<Stay, Long> {
 }
