931eae687e72b35ec6e78bc5108bbdeb18f974b3
yeriimii <87357932+Yeriimii@users.noreply.github.com>
2024-06-13 11:01:46 +0900

    Update src/main/java/com/airdnb/clone/domain/reservation/ReservationRepository.java
    [feat/#13] style: 코드 컨벤션 적용

--- src/main/java/com/airdnb/clone/domain/reservation/ReservationRepository.java
+++ src/main/java/com/airdnb/clone/domain/reservation/ReservationRepository.java
@@ -3,5 +3,6 @@ package com.airdnb.clone.domain.reservation;
 import com.airdnb.clone.domain.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
