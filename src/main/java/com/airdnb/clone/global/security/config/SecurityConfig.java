61d2b5492cd5473578c2a6a1d767332192cb7fef
Yeriimii <seojs0511@naver.com>
2024-06-18 02:14:29 +0900

    [feat/#17] refactor: JwtToken Filter 리팩토링
    - JwtTokenService 적용

--- src/main/java/com/airdnb/clone/global/security/config/SecurityConfig.java
+++ src/main/java/com/airdnb/clone/global/security/config/SecurityConfig.java
@@ -1,15 +1,18 @@
 package com.airdnb.clone.global.security.config;
 
-import com.airdnb.clone.global.security.filter.JWTTokenGeneratorFilter;
-import com.airdnb.clone.global.security.filter.JWTTokenValidatorFilter;
+import com.airdnb.clone.domain.oauth2.AirdnbOAuth2SuccessHandler;
+import com.airdnb.clone.domain.oauth2.AirdnbOAuth2UserService;
+import com.airdnb.clone.domain.oauth2.JwtTokenService;
+import com.airdnb.clone.global.security.filter.JwtTokenGeneratorFilter;
+import com.airdnb.clone.global.security.filter.JwtTokenValidatorFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,7 +22,13 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity(debug = true) // FIXME: 개발 중에만 사용
 public class SecurityConfig {
+
+    private final AirdnbOAuth2UserService userService;
+    private final AirdnbOAuth2SuccessHandler successHandler;
+    private final JwtTokenService jwtTokenService;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -27,29 +36,27 @@ public class SecurityConfig {
                 .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
-                        config.setExposedHeaders(List.of("Authorization"));
-                        config.setMaxAge(3600L);
-                        return config;
+                        CorsConfiguration corsConfig = new CorsConfiguration();
+                        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                        corsConfig.setAllowedMethods(Collections.singletonList("*"));
+                        corsConfig.setAllowedHeaders(Collections.singletonList("*"));
+                        corsConfig.setAllowCredentials(true);
+                        corsConfig.setExposedHeaders(List.of("Authorization"));
+                        corsConfig.setMaxAge(3600L);
+                        return corsConfig;
                     }
-                })).csrf(csrf -> csrf.disable()
-                        .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-                        .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class))
+                })).csrf(csrf -> csrf.disable())
+                .addFilterAfter(new JwtTokenGeneratorFilter(jwtTokenService), BasicAuthenticationFilter.class)
+                .addFilterBefore(new JwtTokenValidatorFilter(jwtTokenService), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/api/v1/reservations/**").hasRole("USER")
-                        .requestMatchers("/members").authenticated()
-                        .requestMatchers("/stays").permitAll())
-                .oauth2Login(new Customizer<OAuth2LoginConfigurer<HttpSecurity>>() {
-                    @Override
-                    public void customize(OAuth2LoginConfigurer<HttpSecurity> httpSecurityOAuth2LoginConfigurer) {
-                        httpSecurityOAuth2LoginConfigurer.loginPage("/auth/login")
-                                .permitAll();
-                    }
-                });
+                        .requestMatchers("/stays").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth2Configurer -> oauth2Configurer
+                        .loginPage("http://localhost:5173/login")
+                        .successHandler(successHandler)
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(userService))
+                );
 
         return http.build();
     }
